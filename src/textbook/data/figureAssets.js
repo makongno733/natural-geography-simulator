2196f8026a87880be03c2f614a0e53aecae6d8e6
makongno <makongno@makongnodeMacBook-Air.local>
2026-08-30 22:58:56 +0800

    feat: refine selective-compulsory-1 textbook content (13 sections + 5 case studies + 34 figures)

--- src/textbook/data/figureAssets.js
+++ src/textbook/data/figureAssets.js
@@ -59,6 +59,41 @@ import remoteSensing from '../../assets/textbook/必修第一册/6.4/remote-sens
 import gnss from '../../assets/textbook/必修第一册/6.4/gnss.jpg'
 import gis from '../../assets/textbook/必修第一册/6.4/gis.jpg'
 
+import self_rotation from '../../assets/textbook/选择性必修1/1.1/self-rotation.jpg'
+import huangchi_angle from '../../assets/textbook/选择性必修1/1.1/huangchi-angle.png'
+import direct_ray_movement from '../../assets/textbook/选择性必修1/1.1/direct-ray-movement.png'
+import day_night from '../../assets/textbook/选择性必修1/1.2/day-night.png'
+import day_length from '../../assets/textbook/选择性必修1/1.2/day-length.jpg'
+import five_zones from '../../assets/textbook/选择性必修1/1.2/five-zones.jpeg'
+import rock_cycle from '../../assets/textbook/选择性必修1/2.1/rock-cycle.jpeg'
+import internal_force from '../../assets/textbook/选择性必修1/2.1/internal-force.png'
+import weathering from '../../assets/textbook/选择性必修1/2.1/weathering.jpg'
+import fold from '../../assets/textbook/选择性必修1/2.2/fold.jpg'
+import fault from '../../assets/textbook/选择性必修1/2.2/fault.png'
+import plates from '../../assets/textbook/选择性必修1/2.2/plates.jpeg'
+import valley_evolution from '../../assets/textbook/选择性必修1/2.3/valley-evolution.jpg'
+import meander from '../../assets/textbook/选择性必修1/2.3/meander.jpeg'
+import alluvial_fan from '../../assets/textbook/选择性必修1/2.3/alluvial-fan.jpg'
+import front from '../../assets/textbook/选择性必修1/3.1/front.png'
+import cyclone from '../../assets/textbook/选择性必修1/3.1/cyclone.png'
+import tri_circulation from '../../assets/textbook/选择性必修1/3.2/tri-circulation.jpeg'
+import pressure_belts from '../../assets/textbook/选择性必修1/3.2/pressure-belts.jpeg'
+import seasonal_movement from '../../assets/textbook/选择性必修1/3.2/seasonal-movement.png'
+import climate_distribution from '../../assets/textbook/选择性必修1/3.3/climate-distribution.jpeg'
+import lake_river from '../../assets/textbook/选择性必修1/4.1/lake-river.png'
+import glacier from '../../assets/textbook/选择性必修1/4.1/glacier.png'
+import ocean_currents from '../../assets/textbook/选择性必修1/4.2/ocean-currents.jpeg'
+import current_pattern from '../../assets/textbook/选择性必修1/4.2/current-pattern.jpg'
+import air_sea_exchange from '../../assets/textbook/选择性必修1/4.3/air-sea-exchange.png'
+import walker from '../../assets/textbook/选择性必修1/4.3/walker.png'
+import el_nino from '../../assets/textbook/选择性必修1/4.3/el-nino.png'
+import la_nina from '../../assets/textbook/选择性必修1/4.3/la-nina.png'
+import elements_exchange from '../../assets/textbook/选择性必修1/5.1/elements-exchange.png'
+import huangshan from '../../assets/textbook/选择性必修1/5.1/huangshan.jpeg'
+import natural_zones from '../../assets/textbook/选择性必修1/5.2/natural-zones.png'
+import vertical_zonation from '../../assets/textbook/选择性必修1/5.2/vertical-zonation.png'
+import latitude_zonation from '../../assets/textbook/选择性必修1/5.2/latitude-zonation.jpg'
+
 export const figureAssets = {
   nebula,
   star,
@@ -118,4 +153,39 @@ export const figureAssets = {
   'remote-sensing': remoteSensing,
   gnss,
   gis,
+
+  'self-rotation': self_rotation,
+  'huangchi-angle': huangchi_angle,
+  'direct-ray-movement': direct_ray_movement,
+  'day-night': day_night,
+  'day-length': day_length,
+  'five-zones': five_zones,
+  'rock-cycle': rock_cycle,
+  'internal-force': internal_force,
+  weathering,
+  fold,
+  fault,
+  plates,
+  'valley-evolution': valley_evolution,
+  meander,
+  'alluvial-fan': alluvial_fan,
+  front,
+  cyclone,
+  'tri-circulation': tri_circulation,
+  'pressure-belts': pressure_belts,
+  'seasonal-movement': seasonal_movement,
+  'climate-distribution': climate_distribution,
+  'lake-river': lake_river,
+  glacier,
+  'ocean-currents': ocean_currents,
+  'current-pattern': current_pattern,
+  'air-sea-exchange': air_sea_exchange,
+  walker,
+  'el-nino': el_nino,
+  'la-nina': la_nina,
+  'elements-exchange': elements_exchange,
+  huangshan,
+  'natural-zones': natural_zones,
+  'vertical-zonation': vertical_zonation,
+  'latitude-zonation': latitude_zonation,
 }
